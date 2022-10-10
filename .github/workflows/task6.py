46ea6b7d528574734ae89a57a3cf0ae403264f33
festus84 <festusaakinyemi84@gmail.com>
2022-10-10 21:03:18 +0100

    add task6

---
+++ .github/workflows/task6.py
@@ -0,0 +1,14 @@
+# Conditional Statement and Logical Operator
+
+a = 30
+b = 60
+lang = "python"
+
+if a >40 and b < 100:
+    print("Evaluation is true")
+
+else:
+    print("Evaluation is false")
+
+    if lang == "python" or "testify":
+        print("Yay !! the string is a testify python")
